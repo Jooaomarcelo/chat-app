5d7fa6b6a1cd1d55feffcf56410ef377f7d2bb2d
João Marcelo <JoaoMdSB@hotmail.com>
2025-01-23 14:59:13 -0300

    added form components and data model

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,7 +1,29 @@
+import 'package:chat_app/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
-class AuthForm extends StatelessWidget {
-  const AuthForm({super.key});
+class AuthForm extends StatefulWidget {
+  final void Function(AuthFormData) onSubmit;
+
+  const AuthForm({
+    required this.onSubmit,
+    super.key,
+  });
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formData = AuthFormData();
+  final _formKey = GlobalKey<FormState>();
+
+  void _submit() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+
+    if (!isValid) return;
+
+    widget.onSubmit(_formData);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,25 +32,54 @@ class AuthForm extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(15),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Nome',
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(),
+              if (_formData.isSignup)
+                TextFormField(
+                  key: ValueKey('name'),
+                  initialValue: _formData.name,
+                  onChanged: (name) => _formData.name = name,
+                  decoration: InputDecoration(
+                    labelText: 'Nome',
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(),
+                    ),
                   ),
+                  validator: (textField) {
+                    final name = textField ?? '';
+
+                    if (name.trim().length < 4) {
+                      return 'O nome deve possuir no mínimo 4 caracteres.';
+                    }
+
+                    return null;
+                  },
                 ),
-              ),
               TextFormField(
+                key: ValueKey('email'),
+                initialValue: _formData.email,
+                onChanged: (email) => _formData.email = email,
                 decoration: InputDecoration(
                   labelText: 'Email',
                   enabledBorder: UnderlineInputBorder(
                     borderSide: BorderSide(),
                   ),
                 ),
+                validator: (textField) {
+                  final email = textField ?? '';
+
+                  if (!email.contains('@')) {
+                    return 'Insira um email válido.';
+                  }
+
+                  return null;
+                },
               ),
               TextFormField(
+                key: ValueKey('password'),
+                initialValue: _formData.password,
+                onChanged: (password) => _formData.password = password,
                 obscureText: true,
                 decoration: InputDecoration(
                   labelText: 'Senha',
@@ -36,21 +87,41 @@ class AuthForm extends StatelessWidget {
                     borderSide: BorderSide(),
                   ),
                 ),
+                validator: (textField) {
+                  final senha = textField ?? '';
+
+                  if (senha.trim().length < 5) {
+                    return 'A senha deve possuir no mínimo 6 caracteres.';
+                  }
+
+                  return null;
+                },
+              ),
+              SizedBox(
+                height: 10,
               ),
               ElevatedButton(
-                onPressed: () {},
+                onPressed: _submit,
                 style: ButtonStyle(
                   backgroundColor: WidgetStateProperty.all(
                       Theme.of(context).colorScheme.primary),
                 ),
                 child: Text(
-                  'Entrar',
+                  _formData.isLogin ? 'Entrar' : 'Cadastrar',
                   style: Theme.of(context).textTheme.titleMedium,
                 ),
               ),
               TextButton(
-                onPressed: () {},
-                child: Text('Criar uma nova conta?'),
+                onPressed: () {
+                  setState(() {
+                    _formData.toogleAuthMode();
+                  });
+                },
+                child: Text(
+                  _formData.isLogin
+                      ? 'Criar uma nova conta?'
+                      : 'Já possui conta?',
+                ),
               ),
             ],
           ),
