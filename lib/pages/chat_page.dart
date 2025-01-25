bd7015e346b5b629771db68418e8bf9c16dc59e7
João Marcelo <JoaoMdSB@hotmail.com>
2025-01-25 14:06:31 -0300

    added message componentes

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/components/messages.dart';
+import 'package:chat_app/components/new_message.dart';
 import 'package:chat_app/core/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -7,15 +9,49 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(
+        title: const Text('Asi Chat'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            itemBuilder: (ctx) => [
+              PopupMenuItem(
+                value: 'logout',
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.exit_to_app,
+                      color: Colors.black87,
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      'Sair',
+                      style: TextStyle(
+                        color: Colors.black87,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+            onSelected: (value) {
+              if (value == 'logout') {
+                AuthService().logout();
+              }
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Chat Page'),
-            TextButton(
-              child: Text('Sair'),
-              onPressed: () => AuthService().logout(),
-            )
+          children: const [
+            Expanded(
+              child: Messages(),
+            ),
+            NewMessage(),
           ],
         ),
       ),
