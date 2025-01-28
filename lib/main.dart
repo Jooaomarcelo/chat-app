d80653329573264781185eb2e5b8a4aebfec76ad
João Marcelo <JoaoMdSB@hotmail.com>
2025-01-28 09:50:29 -0300

    added chat notification provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:chat_app/core/services/notification/chat_notification_service.dart';
 import 'package:chat_app/pages/auth_or_app.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,26 +12,33 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.blueAccent,
-          secondary: Colors.deepOrange,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ChatNotificationService(),
         ),
-        useMaterial3: true,
-        appBarTheme: ThemeData.light().appBarTheme.copyWith(
-              backgroundColor: Colors.blue[900],
-              foregroundColor: Colors.white,
-              centerTitle: true,
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Colors.blueAccent,
+            secondary: Colors.deepOrange,
+          ),
+          useMaterial3: true,
+          appBarTheme: ThemeData.light().appBarTheme.copyWith(
+                backgroundColor: Colors.blue[900],
+                foregroundColor: Colors.white,
+                centerTitle: true,
+              ),
+          textTheme: const TextTheme(
+            titleMedium: TextStyle(
+              color: Colors.white,
             ),
-        textTheme: const TextTheme(
-          titleMedium: TextStyle(
-            color: Colors.white,
           ),
         ),
+        home: const AuthOrApp(),
       ),
-      home: const AuthOrApp(),
     );
   }
 }
