8e3eff641b96f91ef704d4652ea7bb9232d79d29
João Marcelo <JoaoMdSB@hotmail.com>
2025-01-28 11:52:19 -0300

    finished notification page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,7 +1,10 @@
 import 'package:chat_app/components/messages.dart';
 import 'package:chat_app/components/new_message.dart';
 import 'package:chat_app/core/services/auth/auth_service.dart';
+import 'package:chat_app/core/services/notification/chat_notification_service.dart';
+import 'package:chat_app/pages/notification_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({super.key});
@@ -42,6 +45,32 @@ class ChatPage extends StatelessWidget {
                 AuthService().logout();
               }
             },
+          ),
+          Stack(
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (ctx) {
+                      return const NotificationPage();
+                    }),
+                  );
+                },
+                icon: Icon(Icons.notifications),
+              ),
+              Positioned(
+                top: 5,
+                right: 5,
+                child: CircleAvatar(
+                  maxRadius: 10,
+                  backgroundColor: Colors.red.shade800,
+                  child: Text(
+                    '${Provider.of<ChatNotificationService>(context).itemsCount}',
+                    style: TextStyle(fontSize: 10),
+                  ),
+                ),
+              )
+            ],
           ),
         ],
       ),
