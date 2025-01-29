46f1aa9ee1a5a906f2c3d6a848cc685a790c0e3e
João Marcelo <JoaoMdSB@hotmail.com>
2025-01-29 13:32:56 -0300

    added firebase push notification (foreground, background and terminated).

--- lib/pages/auth_or_app_page.dart
+++ lib/pages/auth_or_app_page.dart
@@ -1,9 +1,13 @@
 import 'package:chat_app/core/models/chat_user.dart';
 import 'package:chat_app/core/services/auth/auth_service.dart';
+import 'package:chat_app/core/services/notification/chat_notification_service.dart';
 import 'package:chat_app/pages/auth_page.dart';
 import 'package:chat_app/pages/chat_page.dart';
 import 'package:chat_app/pages/loading_page.dart';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:firebase_core/firebase_core.dart';
 
 class AuthOrAppPage extends StatelessWidget {
@@ -11,6 +15,10 @@ class AuthOrAppPage extends StatelessWidget {
 
   Future<void> init(BuildContext context) async {
     await Firebase.initializeApp();
+    await Provider.of<ChatNotificationService>(
+      context,
+      listen: false,
+    ).init();
   }
 
   @override
